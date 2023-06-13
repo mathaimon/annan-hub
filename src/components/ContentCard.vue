33bd765307769ae7cda5205ade71957ad2372ce1
Naveen Mathew <naveenmathew1585@gmail.com>
2023-06-13 14:47:53 +0530

    ✨ content card connected to firebase

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,16 +1,22 @@
 <script setup>
 import ThumbUpIcon from "vue-material-design-icons/ThumbUp.vue"
+const props = defineProps({
+    content: Object,
+})
 </script>
 <template>
     <div class="w-full py-3">
-        <img src="https://images.pexels.com/photos/296817/pexels-photo-296817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            class="rounded-md object-center h-[14rem] object-cover w-full">
-        <p class="mt-2 text-lg font-semibold text-accent">I was asleep, look what happened</p>
+        <div class="relative">
+            <img :src="props.content.url" loading="lazy" class="rounded-md object-center h-[14rem] object-cover w-full">
+            <div class="absolute bottom-0 right-0 px-2 py-0.5 rounded-tl-md rounded-br-md bg-primary/80">HD <span
+                    class="font-bold">|</span> Image</div>
+        </div>
+        <p class="mt-2 text-lg font-semibold text-accent">{{ props.content.title }}</p>
         <div class="flex items-center mt-1">
-            <p class="mr-5 text-sm text-gray-500">212,314 Views</p>
+            <p class="mr-5 text-sm text-gray-500">{{ props.content.views.toLocaleString() }} Views</p>
             <div class="flex items-center">
                 <ThumbUpIcon fillColor="#39bb1a" :size="18" class="mr-2" />
-                <p class="text-sm font-semibold text-gray-300">92 %</p>
+                <p class="text-sm font-semibold text-gray-300">{{ props.content.like }} %</p>
             </div>
         </div>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,47 @@
 <script setup>
+import { onMounted, ref } from "vue";
+import { db } from "@/firebase/firebase"
+import { collection, onSnapshot } from "firebase/firestore"
+import LoadingIcon from "vue-material-design-icons/Loading.vue"
 import ContentCard from "../components/ContentCard.vue"
+
+const contents = ref([])
+const isLoading = ref(false)
+
+onMounted(() => {
+  isLoading.value = true
+  onSnapshot(collection(db, "content"), (querySnapshot) => {
+    const fbContent = [];
+    querySnapshot.forEach((doc) => {
+      const individualEvent = {
+        id: doc.id,
+        title: doc.data().title,
+        url: doc.data().url,
+        views: doc.data().views,
+        like: doc.data().like
+      };
+      fbContent.push(individualEvent);
+    });
+    contents.value = fbContent;
+    isLoading.value = false
+  });
+});
 </script>
 
 <template>
-  <div class="w-full text-white px-2">
-    <h1 class="mt-5 text-4xl mb-3 font-semibold">Popular Uploads</h1>
+  <div class="w-full px-2 text-white">
+    <h1 class="mt-5 mb-3 text-4xl font-semibold">Popular Uploads</h1>
+    <!-- Warning Dialog Box -->
+    <div class="flex items-center w-full p-4 mt-5 rounded-lg bg-secondary" :class="{ 'hidden': !isLoading }">
+      <div class="w-6 mr-5 animate-spin">
+        <LoadingIcon fillColor="#FFA31A" />
+      </div>
+      <p>Please wait while we load awersome Annan content <span class="animate-pulse">....</span>
+      </p>
+    </div>
     <!-- Announcement Card -->
-    <div class="mt-4 flex flex-col gap-4">
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
+    <div class="flex flex-col gap-4 mt-4" :class="{ 'hidden': isLoading }">
+      <ContentCard v-for="content in contents" :key="content.id" :content="content" />
     </div>
   </div>
 </template>
