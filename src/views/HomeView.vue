6ab9f539af39d02bfb607626fe7a0285abfe9c6b
Naveen Mathew <naveenmathew1585@gmail.com>
2023-06-13 09:38:06 +0530

    ✨ add content card placeholders

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <script setup>
+import ContentCard from "../components/ContentCard.vue"
 </script>
 
 <template>
-  <div class="bg-primary text-accent text-3xl">Hello World</div>
+  <div class="w-full text-white px-2">
+    <h1 class="mt-5 text-4xl mb-3 font-semibold">Popular Uploads</h1>
+    <!-- Announcement Card -->
+    <div class="mt-4 flex flex-col gap-4">
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+      <ContentCard />
+    </div>
+  </div>
 </template>
