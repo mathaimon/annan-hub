5e924b030cbdb273e256e60224a767ee0a11c0d5
Naveen Mathew <naveenmathew1585@gmail.com>
2023-06-13 09:37:24 +0530

    💄 updated display font

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,11 @@ import NavBar from './components/NavBar.vue';
     <RouterView class="max-w-md mx-auto pt-14" />
   </div>
 </template>
+<style>
+@import url("https://api.fontshare.com/v2/css?f[]=satoshi@1,900,700,500,301,701,300,501,401,901,400,2&display=swap");
+
+body {
+  font-family: "Satoshi", sans-serif;
+}
+</style>
+
