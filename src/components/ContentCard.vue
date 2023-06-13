b5e2be089ddd044a0ede635c0f46de8c5868bc70
Naveen Mathew <naveenmathew1585@gmail.com>
2023-06-13 09:43:25 +0530

    💄 updated font size

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -5,12 +5,12 @@ import ThumbUpIcon from "vue-material-design-icons/ThumbUp.vue"
     <div class="w-full py-3">
         <img src="https://images.pexels.com/photos/296817/pexels-photo-296817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             class="rounded-md object-center h-[14rem] object-cover w-full">
-        <p class="text-accent mt-2 text-lg font-semibold">I was asleep, look what happened</p>
-        <div class="flex mt-1 items-center">
-            <p class="text-gray-500 mr-5 text-sm">212,314 Views</p>
+        <p class="mt-2 text-lg font-semibold text-accent">I was asleep, look what happened</p>
+        <div class="flex items-center mt-1">
+            <p class="mr-5 text-sm text-gray-500">212,314 Views</p>
             <div class="flex items-center">
-                <ThumbUpIcon fillColor="#39bb1a" :size="20" class="mr-2" />
-                <p class="text-gray-300 font-semibold">92 %</p>
+                <ThumbUpIcon fillColor="#39bb1a" :size="18" class="mr-2" />
+                <p class="text-sm font-semibold text-gray-300">92 %</p>
             </div>
         </div>
     </div>
